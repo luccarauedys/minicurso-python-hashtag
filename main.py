16dc42e1d7546ab7b819d120d0587d2b799f2938
Lucca Rauédys <luccarauedys@outlook.com>
2023-05-22 19:04:00 -0300

    calcular o produto com maior faturamento

--- main.py
+++ main.py
@@ -9,19 +9,28 @@ for arquivo in lista_de_arquivos:
     if "vendas" in arquivo.lower():
         caminho_do_arquivo = f"./vendas/{arquivo}"
         tabela = pd.read_csv(caminho_do_arquivo)
-        # Juntando todas as tabelas de vendas em uma só
         tabela_de_vendas_completa = pd.concat(
             [tabela_de_vendas_completa, tabela])
 
-# Calculando o produto mais vendido (em quantidade):
+# 1. Calculando o produto mais vendido (em quantidade):
 
-# Agrupando por nome do produto e somando as demais colunas
 tabela_de_produtos = tabela_de_vendas_completa.groupby("Produto").sum()
-# Pegando somente a coluna que somou as quantidades vendidas de cada produto
 tabela_de_produtos = tabela_de_produtos[["Quantidade Vendida"]]
-# Ordenando do produto mais vendido para o menos vendido
 tabela_de_produtos = tabela_de_produtos.sort_values(
     by="Quantidade Vendida", ascending=False)
-# Pegando o nome do produto mais vendido
+
 produto_mais_vendido = tabela_de_produtos.index[0]
-print(produto_mais_vendido)
+print(f"PRODUTO MAIS VENDIDO: {produto_mais_vendido}")
+
+# 2. Calculando o produto com maior faturamento
+
+tabela_de_vendas_completa["Faturamento"] = tabela_de_vendas_completa["Quantidade Vendida"] * \
+    tabela_de_vendas_completa["Preco Unitario"]
+
+tabela_faturamento = tabela_de_vendas_completa.groupby("Produto").sum()
+tabela_faturamento = tabela_faturamento[["Faturamento"]]
+tabela_faturamento = tabela_faturamento.sort_values(
+    by="Faturamento", ascending=False)
+
+produto_maior_faturamento = tabela_faturamento.index[0]
+print(f"PRODUTO COM MAIOR FATURAMENTO: {produto_maior_faturamento}")
