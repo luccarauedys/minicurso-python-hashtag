37e636807047317bc2168c7b1e8379fc9bd9bdfd
Lucca Rauédys <luccarauedys@outlook.com>
2023-05-22 19:24:36 -0300

    add requirements.txt

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ for arquivo in lista_de_arquivos:
         tabela_de_vendas_completa = pd.concat(
             [tabela_de_vendas_completa, tabela])
 
-# 1. Calculando o produto mais vendido (em quantidade):
+# Calculando o produto mais vendido (em quantidade):
 
 tabela_de_produtos = tabela_de_vendas_completa.groupby("Produto").sum()
 tabela_de_produtos = tabela_de_produtos[["Quantidade Vendida"]]
@@ -22,7 +22,7 @@ tabela_de_produtos = tabela_de_produtos.sort_values(
 produto_mais_vendido = tabela_de_produtos.index[0]
 print(f"PRODUTO MAIS VENDIDO: {produto_mais_vendido}")
 
-# 2. Calculando o produto com maior faturamento
+# Calculando o produto com maior faturamento
 
 tabela_de_vendas_completa["Faturamento"] = tabela_de_vendas_completa["Quantidade Vendida"] * \
     tabela_de_vendas_completa["Preco Unitario"]
@@ -35,7 +35,7 @@ tabela_faturamento = tabela_faturamento.sort_values(
 produto_maior_faturamento = tabela_faturamento.index[0]
 print(f"PRODUTO COM MAIOR FATURAMENTO: {produto_maior_faturamento}")
 
-# 3. Calcular a loja com maior faturamento
+# Calculando a loja com maior faturamento
 
 tabela_lojas = tabela_de_vendas_completa.groupby("Loja").sum()
 tabela_lojas = tabela_lojas[["Faturamento"]]
